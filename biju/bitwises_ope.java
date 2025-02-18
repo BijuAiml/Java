40f7375791ed74293fd53045db951c170a2febf8
Crazybiju <biju.aiml@gmail.com>
2025-02-19 00:39:54 +0530

    create a new  file

---
+++ biju/bitwises_ope.java
@@ -0,0 +1,15 @@
+
+public class bitwise_ope {
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 15;
+        int c = a ^ b;
+        int d = a | b;
+        int e = a & b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
--- biju/pattens.java
+++ biju/pattens.java
@@ -2,7 +2,7 @@
 public class pattens {
 
     public static void main(String args[]) {
-        int arr[] = {1, 2, 3, 4, 5, 6};
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int num = 3;
         for (int j = num; j >= 1; j--) {
             System.out.print(j);
