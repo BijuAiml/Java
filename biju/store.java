113c47ea7c3944305a61d4e734ad8c60201aa2e7
Crazybiju <biju.aiml@gmail.com>
2025-02-19 19:53:39 +0530

    create a new file

---
+++ biju/store.java
@@ -0,0 +1,11 @@
+
+public class store {
+
+    public static void main(string[] args) {
+        int value = 30;
+        String value2 = "victer";
+        System.out.println("values");
+        System.out.println("value2");
+
+    }
+}
