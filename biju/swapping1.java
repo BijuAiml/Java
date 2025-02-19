37d058a4aa7974f6a81625ee607163b40e45bc57
Crazybiju <biju.aiml@gmail.com>
2025-02-19 22:46:16 +0530

    create a new fille

---
+++ biju/swapping1.java
@@ -0,0 +1,14 @@
+
+public class swapping1 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        System.out.println(+a + " " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(+a + "" + b);
+
+    }
+}
