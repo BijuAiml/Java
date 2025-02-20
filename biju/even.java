dfe57c6142201008d9307382a125d6854c9c2498
Crazybiju <biju.aiml@gmail.com>
2025-02-20 20:05:28 +0530

    modify the code

---
+++ biju/even.java
@@ -0,0 +1,13 @@
+
+public class even {
+
+    public static double add(double a, double b) {
+        double c = a + b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        double result = add(234.09, 600.88);
+        System.out.print(result);
+    }
+}
