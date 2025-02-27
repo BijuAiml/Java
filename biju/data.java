00d2596f89366716a1c566adbaca0094d45cdf3e
Crazybiju <biju.aiml@gmail.com>
2025-02-27 23:03:17 +0530

    create a new file

--- biju/data.java
+++ biju/data.java
@@ -1,10 +1,26 @@
 
-import java.util.Date;
-
 public class data {
 
     public static void main(String[] args) {
-        Date today = new Date();
-        System.out.println(today);
+        int n = 4;
+        // int count = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ biju/read.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class read {
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("enter the integer");
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        System.out.println("enter the integer:" + num);
+        s.close();
+
+    }
+}
